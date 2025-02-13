f16a27851ab2fed11ec95342f3fb37e329d6d9f6
oscargnu <oscareduardozambrano@gmail.com>
2025-02-13 17:13:00 +0000

    Creation and abstraction of domain models

--- app/src/main/java/com/juandgaines/todoapp/domain/Task.kt
+++ app/src/main/java/com/juandgaines/todoapp/domain/Task.kt
@@ -1,9 +1,11 @@
 package com.juandgaines.todoapp.domain
 
 data class Task (
+    val tasks: MutableList<Task> = mutableListOf(),
+    val id: String,
     val title: String,
     val description: String?,
     val isCompleted: Boolean = false,
-    val category: Category? =null
+    val category: Category? = null,
 
 )
--- app/src/main/java/com/juandgaines/todoapp/domain/TaskLocalDataSource.kt
+++ app/src/main/java/com/juandgaines/todoapp/domain/TaskLocalDataSource.kt
@@ -1,8 +1,14 @@
 package com.juandgaines.todoapp.domain
 
-interface TaskLocalDataSource {
-    fun addTask(task: Task)
-    fun updateTask (task: Task)
-    fun removeTask (task: Task)
-    fun deleAllTask (task: Task)
+
+
+import kotlinx.coroutines.flow.Flow
+
+interface TaskLocalDataSource{
+   val tasksFlow: Flow<List<Task>>
+   suspend fun addTask(task: Task)
+   suspend fun updateTask(updatedTask: Task)
+   suspend fun removeTask(task: Task)
+   suspend fun deleteAllTasks()
+   suspend fun getTaskById(taskId: String): Task?
 }
